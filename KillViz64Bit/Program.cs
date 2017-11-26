7912db5ba20baa3e95a0bff9f778423ed46c5993
Jakob Viggo <jakobviggohansen@gmail.com>
2017-11-26 15:29:53 +0100

    Initial commit of KillViz 64Bit

---
+++ KillViz64Bit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillViz64Bit
+{
+    class Program
+    {
+        static string[] killViz = new string[] { "viz", "VizGui" };
+
+        static void Main(string[] args)
+        {
+            foreach (var viz in killViz)
+            {
+                Process[] processNames = Process.GetProcessesByName(viz).ToArray();
+
+                // kill viz
+                foreach (Process process in processNames)
+                {
+                    //uncomment for testing
+                    //Console.WriteLine(process);
+                    process.Kill();
+                }
+            }
+            //uncomment for testing
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+    }
+}
